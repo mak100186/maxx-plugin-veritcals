7f029596907320e6d093d10b41dad3bc063610cb
Muhammed Khan <muhammed.khan@kindredgroup.com>
2023-09-15 11:53:37 +1000

    ensure migrations are working

--- src/Maxx.PluginVerticals.Shared/Entities/Article.cs
+++ src/Maxx.PluginVerticals.Shared/Entities/Article.cs
@@ -4,6 +4,8 @@ public class Article
 {
     public Guid Id { get; set; }
 
+    public int Stars { get; set; }
+
     public string Title { get; set; } = string.Empty;
 
     public string Content { get; set; } = string.Empty;
---
+++ src/Maxx.PluginVerticals.Shared/Migrations/20230915015258_AddedStars.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Maxx.PluginVerticals.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedStars : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Stars",
+                table: "Articles",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Stars",
+                table: "Articles");
+        }
+    }
+}
